a035eab012377c04f9ec61d28537024e068bc7d3
olalekanlukman <79770609+olalekanlukman@users.noreply.github.com>
2023-09-10 18:44:03 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,20 +3,19 @@
 
 
 def minOperations(n):
-    if n == 1:
-        return 0
-    if n <= 0 or n % 1 != 0:
-        return 0
-
-    operations = 0
-    i = 2
-
-    while n > 1:
-        while n % i == 0:
-            operations += i
-            n = n // i
-        i += 1
-
-    return operations
-
-
+    if n <= 1:
+        return n
+    
+    # Initialize an array to store the minimum operations required for each position
+    dp = [0] * (n + 1)
+    
+    # Start from position 2 (first H already exists)
+    for i in range(2, n + 1):
+        dp[i] = i  # Default to just pasting i times
+        
+        # Try all possible factors of i
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+    
+    return dp[n]
