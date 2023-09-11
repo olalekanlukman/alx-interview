c308284feea660334934d65b1cee52d5fa963ae9
olalekanlukman <79770609+olalekanlukman@users.noreply.github.com>
2023-09-11 08:50:16 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
 """ Module for 0-minoperations"""
 
-
 def minOperations(n):
     """
     minOperations
     Gets fewest # of operations needed to result in exactly n H characters
     """
-    H_count = 1
-    copy = 0
-    command_count = 0
+    if n == 1:
+        return 0
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
+    return n
 
-    for i in range(0, n):
 
-        if H_count == n:
-            return command_count
-        if n % H_count == 0:
-            copy = H_count
-            H_count += copy
-            command_count += 2
-        else:
-            H_count += copy
-            command_count += 1
-    return command_count
+
+# def minOperations(n):
+#     """
+#     minOperations
+#     Gets fewest # of operations needed to result in exactly n H characters
+#     """
+#     H_count = 1
+#     copy = 0
+#     command_count = 0
+
+#     for i in range(0, n):
+
+#         if H_count == n:
+#             return command_count
+#         if n % H_count == 0:
+#             copy = H_count
+#             H_count += copy
+#             command_count += 2
+#         else:
+#             H_count += copy
+#             command_count += 1
+#     return command_count
