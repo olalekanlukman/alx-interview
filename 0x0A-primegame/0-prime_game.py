2d613f29829d86e462db4cad2627f85a04f9b202
olalekanlukman <79770609+olalekanlukman@users.noreply.github.com>
2023-09-12 18:33:59 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,42 +2,41 @@
 """0. Prime Game - Maria and Ben are playing a game"""
 
 
-
-
 def isWinner(x, nums):
-    # Function to check if a number is prime
-    def is_prime(n):
-        if n < 2:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
-
-    # Function to play the game
-    def play_game(n):
-        primes = [i for i in range(2, n + 1) if is_prime(i)]
-        turn = 0  # 0 for Maria, 1 for Ben
-        while primes:
-            prime = primes.pop(0)
-            primes = [p for p in primes if p % prime != 0]
-            turn = 1 - turn
-        return "Maria" if turn else "Ben"
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        winner = play_game(n)
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """x - rounds
+    nums - numbers list
+    """
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
         return None
 
+    ben = 0
+    maria = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
