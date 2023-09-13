64c5b3b881468ad452ee59d14fdbba4369d28977
olalekanlukman <79770609+olalekanlukman@users.noreply.github.com>
2023-09-13 07:42:55 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,21 +3,23 @@
 """ Contains makeChange function"""
 
 
-from typing import List
-
-def makeChange(coins: List[int], total: int) -> int:
+def makeChange(coins, total):
     """
     Returns: fewest number of coins needed to meet total
         If total is 0 or less, return 0
         If total cannot be met by any number of coins you have, return -1
     """
+    if total <= 0:
+        return 0
+
+    # Initialize the dp table with infinity except for the first element
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    
-    # Build up dp array
+
+    # Loop through the total, and for each coin, update the dp table
     for i in range(1, total + 1):
-        for c in coins:
-            if i - c >= 0 and dp[i - c] != -1:
-                dp[i] = min(dp[i], dp[i - c] + 1)
-    
+        for coin in coins:
+            if i - coin >= 0 and dp[i - coin] != float('inf'):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
     return dp[total] if dp[total] != float('inf') else -1
