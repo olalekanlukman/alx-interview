c35e4a02894f38202c399686afe7a4da0823eb7b
olalekanlukman <79770609+olalekanlukman@users.noreply.github.com>
2023-09-13 07:24:04 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,26 +2,24 @@
 
 """ Contains makeChange function"""
 
-
 def makeChange(coins, total):
-    """
+     """
     Returns: fewest number of coins needed to meet total
         If total is 0 or less, return 0
         If total cannot be met by any number of coins you have, return -1
     """
     if total <= 0:
         return 0
-
-    # Initialize a list with values set to a large number (greater than the maximum possible value)
-    dp = [float('inf')] * (total + 1)
     
-    # Base case: 0 coins are needed to make change for amount 0
+    # Initialize the dp array with inf for all values except for 0
+    dp = [float('inf')] * (total + 1)
     dp[0] = 0
-
-    # Iterate through the coins and the possible totals
-    for coin in coins:
-        for i in range(coin, total + 1):
-            # Update the minimum number of coins needed to make change for amount i
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
+    
+    # Loop through the total amount
+    for i in range(1, total + 1):
+        # For each coin, update the dp value
+        for c in coins:
+            if i - c >= 0 and dp[i - c] != float('inf'):
+                dp[i] = min(dp[i], dp[i - c] + 1)
+    
     return dp[total] if dp[total] != float('inf') else -1
